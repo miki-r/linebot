6f85954f79e26a04041613ed9a82a63df28acfde
Ryoga Miki <r.y.mz0601@gmail.com>
2019-11-11 11:41:13 +0900

    make it better

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -37,7 +37,8 @@ public class EchoApplication {
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
-        final String originalMessageText = event.getMessage().getText();
+        final String originalMessageText = "a";
+        		//event.getMessage().getText();
         return new TextMessage(originalMessageText);
     }
 

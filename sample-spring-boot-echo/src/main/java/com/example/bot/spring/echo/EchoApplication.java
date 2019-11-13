00fb63872d5b8bea09637e5ffd472158a68d1c3c
Ryoga Miki <r.y.mz0601@gmail.com>
2019-11-13 17:23:25 +0900

    make it better

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -1,50 +1,19 @@
-/*
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.example.bot.spring.echo;
 
-import java.util.concurrent.ExecutionException;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.ReplyMessage;
-import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.message.template.ConfirmTemplate;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
-    @Autowired
-    private LineMessagingClient lineMessagingClient;
-
-    private static final String reply = "はい";
-
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
@@ -52,49 +21,12 @@ public class EchoApplication {
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
-        String originalMessageText = event.getMessage().getText();
-        String replyToken = event.getReplyToken();
-
-        if (reply.equals(originalMessageText)) {
-            originalMessageText = "承知いたしました";
-        }
-
-        switch (originalMessageText) {
-            case "こんにちは": {
-                ConfirmTemplate confirmTemplate = new ConfirmTemplate(
-                        "こんにちは～！元気？",
-                        new MessageAction("元気！！", "元気！！"),
-                        new MessageAction("微妙", "微妙")
-                );
-
-                TemplateMessage templateMessage = new TemplateMessage("Confirm alt text", confirmTemplate);
-                this.reply(replyToken, templateMessage);
-                break;
-            }
-            case "元気！！": {
-                originalMessageText = "それはよかったね";
-                break;
-            }
-            case "微妙": {
-                originalMessageText = "あら・・・大丈夫？";
-                break;
-            }
-        }
+        final String originalMessageText = event.getMessage().getText();
         return new TextMessage(originalMessageText);
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
-    }
-
-    private void reply(@NonNull String replyToken, TemplateMessage templateMessage) {
-        try {
-            BotApiResponse apiResponse = lineMessagingClient
-                    .replyMessage(new ReplyMessage(replyToken, templateMessage))
-                    .get();
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
